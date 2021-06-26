20fa4f31f2d0a75750bffa304a6399e93c513cee
alexn400 <alexn400@gmail.com>
2021-06-27 10:04:34 +1200

    add linting setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
+interface Data {
   name: string
 }
 
