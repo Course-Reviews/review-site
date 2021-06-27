eb6052cbe3187524f68f3e896df2bf0554db103c
alexn400 <alexn400@gmail.com>
2021-06-27 13:09:58 +1200

    Add basic API client

---
+++ util/APIClient.ts
@@ -0,0 +1,29 @@
+export interface CourseSummary {
+  id: string;
+  name: string;
+  uni: string;
+  rating: number;
+}
+
+export interface Review {
+  id: string;
+  dateCreated: Date;
+  name: string;
+  takenDate: string;
+  upvotes: number;
+  downvotes: number;
+  content?: string;
+  rating: number;
+}
+
+export default class Client {
+  static token?: string;
+
+  static setToken(token: string) {
+    this.token = token;
+  }
+
+  static clearToken() {
+    delete this.token;
+  }
+}
