f82bdf98ece7f088ad48a99622d6d06831610054
Michael Shaimerden <sanzharshaimerden@gmail.com>
2021-06-29 21:53:43 +1200

    Set up mixpanel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from '../components/atom/Container';
 import Navbar from '../components/atom/Navbar';
 import { ModalProvider, useModal } from 'async-modals';
@@ -14,39 +14,44 @@ import ScrollToTop from '../components/ScrollToTop';
 import NavSearch from '../components/NavSearch';
 import { isMobile } from 'react-device-detect';
 import Footer from '../components/Footer';
+import mixpanel from 'mixpanel-browser';
+import { MixpanelProvider, MixpanelConsumer } from 'react-mixpanel';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Mobile search opens the modal instead
-  console.log(isMobile);
+  mixpanel.init('08d4d7028dcc32f1449375dc93c154c7');
+  console.log(mixpanel);
   return (
-    <ModalProvider
-      backgroundClassName={(isExiting) =>
-        classNames(
-          'fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center z-40',
-          isExiting ? 'animate-modal-bg-fade-out' : 'animate-modal-bg-fade-in'
-        )
-      }
-      exitDelay={200}
-    >
-      <div className='bg-gray-50 min-h-screen select-none'>
-        <Navbar>
-          <Container className={'flex justify-between'}>
-            <Link href='/'>
-              <a className={'flex items-center'}>
-                <FiFeather className={'text-primary-500 mr-4'} size={32} />
-                Review App
-              </a>
-            </Link>
-            <div className={`flex items-center ${isMobile ? 'w-2/3' : 'w-1/3'} justify-end`}>
-               <SearchButton className={'sm:hidden'} /> <NavSearch className={'hidden sm:flex'} />
-            </div>
-          </Container>
-        </Navbar>
-        <Component {...pageProps} />
-        <Footer />
-        <ScrollToTop />
-      </div>
-    </ModalProvider>
+    <MixpanelProvider mixpanel={mixpanel}>
+      <ModalProvider
+        backgroundClassName={(isExiting) =>
+          classNames(
+            'fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center z-40',
+            isExiting ? 'animate-modal-bg-fade-out' : 'animate-modal-bg-fade-in'
+          )
+        }
+        exitDelay={200}
+      >
+        <div className='bg-gray-50 min-h-screen select-none'>
+          <Navbar>
+            <Container className={'flex justify-between'}>
+              <Link href='/'>
+                <a className={'flex items-center'}>
+                  <FiFeather className={'text-primary-500 mr-4'} size={32} />
+                  Review App
+                </a>
+              </Link>
+              <div className={`flex items-center ${isMobile ? 'w-2/3' : 'w-1/3'} justify-end`}>
+                <SearchButton className={'sm:hidden'} /> <NavSearch className={'hidden sm:flex'} />
+              </div>
+            </Container>
+          </Navbar>
+          <Component {...pageProps} />
+          <Footer />
+          <ScrollToTop />
+        </div>
+      </ModalProvider>
+    </MixpanelProvider>
   );
 };
 
