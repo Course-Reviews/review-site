17872c32f2042ce58a046a1c059adf23d547a992
alexn400 <alexn400@gmail.com>
2021-06-30 19:08:36 +1200

    fix parallax

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -6,14 +6,14 @@ interface ParallaxProps extends HTMLAttributes<HTMLElement>{
 
 const Parallax: React.FC<ParallaxProps> = ({...rest}) => {
 
-  const [offset, setOffset] = useState(-50);
+  const [offset, setOffset] = useState(0);
 
   useEffect(() => {
 
     const listener = () => {
       const scrollY = window.scrollY;
       console.log(scrollY);
-      setOffset(scrollY/2 - 50)
+      setOffset(scrollY/2 )
     }
     document.addEventListener('scroll', listener)
     return () => document.removeEventListener('scroll', listener)
