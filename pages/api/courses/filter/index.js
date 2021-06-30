e857cc1b7f6ace7a606c2d53d36894bceb2fa1e9
Jia Qi <04jiaqi@gmail.com>
2021-06-30 13:37:05 +1200

    Update search, limit by 5 and default sort by code

---
+++ pages/api/courses/filter/index.js
@@ -0,0 +1,29 @@
+import connectDB from '../../../../db/mongoose';
+import Course from '../../../../models/course';
+
+connectDB();
+
+const handler = async (req, res) => {
+  if (req.method === 'GET') {
+    const { university, stage } = req.query;
+    const match = {};
+
+    if (university) {
+      match.university = university;
+    }
+
+    if (stage) {
+      match.stage = stage;
+    }
+
+    try {
+      const courses = await Course.find(match);
+
+      res.status(200).json(courses);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  }
+};
+
+export default handler;
