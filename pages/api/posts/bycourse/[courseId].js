a4ec57ba06bd3f3e345ea04442509a89ba99f276
Jia Qi <04jiaqi@gmail.com>
2021-07-04 12:06:36 +1200

    added overall rating and number of ratings to send

--- pages/api/posts/bycourse/[courseId].js
+++ pages/api/posts/bycourse/[courseId].js
@@ -41,12 +41,27 @@ const handler = async (req, res) => {
   if (req.method === 'GET') {
     // Fetch all reviews for a given course
 
+    let number = 0;
+    let overall = 0;
+
     try {
       const reviews = await Review.find({
         owner: req.query.courseId,
         content: {$exists:true}
+      }, (err, results) => {
+          if(results) {
+            for(var i = 0; i < results.length; i++) {
+              overall += results[i].course_rating;
+            }
+            number = results.length;
+          }
       });
-      res.status(200).json(reviews);
+
+      if(number > 0) {
+        overall /= number;
+      }
+
+      res.status(200).json({ no_of_ratings: number, overall_rating: overall, reviews});
     } catch (e) {
       res.status(400).json(e.errors.content.properties.message);
     }
