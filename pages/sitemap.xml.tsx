2125e72ecec8d78c0f0e03db4c297f0ac8d238db
Michael Shaimerden <sanzharshaimerden@gmail.com>
2021-07-04 12:59:48 +1200

    Setup sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,36 @@
+import courseList from '../util/courseList.json';
+
+const Sitemap: React.FC = () => {};
+
+export const getServerSideProps = ({ res }) => {
+  const baseUrl = 'https://coursereview.co.nz/courses';
+  const staticPages = courseList.map((staticPagePath) => `${baseUrl}/${staticPagePath}`);
+
+  console.log(staticPages);
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map(
+          (url) => `
+      <url>
+        <loc>${url}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+    `
+        )
+        .join('')}
+    </urlset>
+  `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
