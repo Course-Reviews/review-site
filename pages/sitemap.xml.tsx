b3d123ab960ffdf1f10735299c8393b3718aef4c
Michael Shaimerden <sanzharshaimerden@gmail.com>
2021-07-05 13:23:24 +1200

    update sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -3,7 +3,7 @@ import courseList from '../util/courseList.json';
 
 const Sitemap: React.FC = () => <></>;
 
-export const getServerSideProps: GetServerSideProps  = async ({ res }) => {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const baseUrl = 'https://coursereview.co.nz/courses';
   const staticPages = courseList.map((staticPagePath) => `${baseUrl}/${staticPagePath}`);
 
@@ -11,6 +11,12 @@ export const getServerSideProps: GetServerSideProps  = async ({ res }) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+    <loc>https://coursereview.co.nz</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
       ${staticPages
         .map(
           (url) => `
